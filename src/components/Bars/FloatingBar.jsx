c49fcbf3bf0650e6ae81d401522c733a08d696a4
luffy-hh <141474915+luffy-hh@users.noreply.github.com>
2024-08-08 00:52:45 -0700

    more responsive

--- src/components/Bars/FloatingBar.jsx
+++ src/components/Bars/FloatingBar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "./floatingBar.scss";
 import CustomButton from "../Buttons/CustomButton";
+import { useNavigate } from "react-router";
 const FloatingBar = () => {
+  const nav = useNavigate();
+  const playNow = () => {
+    nav("/wheel");
+  };
+  const winnerList = () => {
+    nav("/winners");
+  };
   return (
     <div className="float-bar bar">
-      <CustomButton text="Play Now" className={`btn btn-play`} />
-      <CustomButton text="Vip Promotion" className={`btn btn-vip`} />
+      <CustomButton
+        text="Play Now"
+        className={`btn btn-play`}
+        onClickFun={playNow}
+      />
+      <CustomButton
+        text="Recent Winners"
+        className={`btn btn-vip`}
+        onClickFun={winnerList}
+      />
     </div>
   );
 };
--- src/components/Buttons/CustomButton.jsx
+++ src/components/Buttons/CustomButton.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const CustomButton = (props) => {
-  const handleClick = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
     props?.onClickFun();
   };
 
